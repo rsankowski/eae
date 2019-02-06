9d1dbb62bc69d44ddf4141c87910a425696be9f5
rsankowski <romansankowski@gmail.com>
2019-02-06 17:10:46 +0100

    removed most of the interactivity

--- server.R
+++ server.R
@@ -8,12 +8,6 @@
 load('20190131-mouse-eae-data.Robj')
 colors_many <- c("#E6194B","#3CB44B","#FFE119","#0082C8","#F58231","#911EB4","#46F0F0","#F032E6","#D2F53C","#FABEBE","#008080","#E6BEFF","#AA6E28","#FFFAC8","#800000","#AAFFC3","#808000","#FFD8B1","#000080","#808080","#FFFFFF","#000000","#8DD3C7","#FFFFB3","#BEBADA","#FB8072","#80B1D3","#FDB462","#B3DE69","#FCCDE5","#D9D9D9","#BC80BD","#CCEBC5")
 
-#createLink <- function(val) {
-#  sprintf('<a href="https://www.ncbi.nlm.nih.gov/gene?term=(%s[gene]) AND (Mus musculus[orgn]) AND alive[prop] NOT newentry[gene]&sort=weight" target="_blank" >%s</a>',val,val)  #https://www.ncbi.nlm.nih.gov/gene?term=(cx3cr1[gene])%20AND%20(Mus%20musculus[orgn])%20AND%20alive[prop]%20NOT%20newentry[gene]&sort=weight
-#}
-ToLink <- function(txt) {
-  paste0('<a href="https://www.ncbi.nlm.nih.gov/gene?term=(' ,txt,'[gene]) AND (Mus musculus[orgn]) AND alive[prop] NOT newentry[gene]&sort=weight" target="_blank" >%s</a>')
-}
 
 shinyServer(function(input, output) {
         
@@ -31,10 +25,11 @@ shinyServer(function(input, output) {
         
         #Plot Tsne with group labels
         output$tsnePlotVar <- renderPlotly({
+          .type = "seq"
+          .palette = "YlGnBu"
+          variable = data[[input$dataset]][[1]][[input$plot_variable]]
+          
                 if (is.numeric(data[[input$dataset]][[1]][[input$plot_variable]])) {
-                        .type = "seq"
-                        .palette = "YlGnBu"
-                        variable = data[[input$dataset]][[1]][[input$plot_variable]]
                         vars <- ggplot(data[[input$dataset]][[1]], aes(V1, V2, fill = variable)) +
                                 geom_point(pch = 21, size = 2, stroke = 0.1) +
                                 theme_void() +
@@ -43,22 +38,19 @@ shinyServer(function(input, output) {
                         ggplotly(vars)
                 }
                 else {
-                        colors_many <- c("#E6194B","#3CB44B","#FFE119","#0082C8","#F58231","#911EB4","#46F0F0","#F032E6","#D2F53C","#FABEBE","#008080","#E6BEFF","#AA6E28","#FFFAC8","#800000","#AAFFC3","#808000","#FFD8B1","#000080","#808080","#FFFFFF","#000000","#8DD3C7","#FFFFB3","#BEBADA","#FB8072","#80B1D3","#FDB462","#B3DE69","#FCCDE5","#D9D9D9","#BC80BD","#CCEBC5")
-                        variable = data[[input$dataset]][[1]][[input$plot_variable]]
-                        ggplot(data[[input$dataset]][[1]], aes(V1, V2, fill = variable)) +
+                        vars <- ggplot(data[[input$dataset]][[1]], aes(V1, V2, fill = variable)) +
                                 geom_point(pch = 21, size = 2, stroke = 0.1) +
                                 theme_void() +
                                 scale_fill_manual(input$plot_variable,values = colors_many) +
                                 labs(title = input$plot_variable)
+                        ggplotly(vars)
                 }
                 
                 
         })
-       
+        
         #Plot tsne plot Gene expression
         output$tsneExpPlot <- renderPlot({
-                        #gene <- c()
-                        #.gene <- for (i in length(input$gene)) {paste(gene, input$gene[i], sep = ', ')}
                         l <- (rowSums(as.matrix(data[[input$dataset]][[2]][, colnames(data[[input$dataset]][[2]]) %in% unique(unlist(stringr::str_split(input$gene, c(',',', ', ' ', ' , '))))]))) + 0.1
                         mi <- min(l)
                         ma <- max(l)
@@ -73,7 +65,7 @@ shinyServer(function(input, output) {
                                 geom_point(size = 3, pch = 21, stroke=0.25) +
                                 scale_fill_gradientn('', colors = ColorRamp) +
                                 theme_void() +
-                                labs(title = paste(input$gene, collapse=', '))
+                                labs(title = paste0('Gene expression: ' ,paste(input$gene, collapse=', ')))
                 
                         expr
         })
@@ -127,28 +119,12 @@ shinyServer(function(input, output) {
                         
                         datatable(
                           a[a$fc>1,], selection = 'none', class = 'cell-border strip hover', options = list(autoWidth = TRUE)
-                        ) %>% formatStyle(0, cursor = 'pointer') %>% 
+                        ) %>% #formatStyle(0, cursor = 'pointer') %>% 
                           formatRound(c(1:5), 2) 
                         
         })
         
-        observeEvent(input$clusterTableUp_cell_clicked, {
-          info = input$clusterTableUp_cell_clicked
-          
-          if (!is.null(info$value) ) { # Not null means we have a legitimate address
-            
-            addr = info$value
-            
-            ToLink(addr)
-            # Here you need to convert this addr data into the search parameter and send off to google and present the results somewhere
-            
-            # So you will need to invoke a search (probably easy but I dont know api but see example for using a  url shortening AI here  https://mark.shinyapps.io/googleAuthRexample/
-            
-            # Good luck !
-            
-          }
-          
-        })
+        
         
         #Plot downregulated genes
         output$clusterTableDown <- renderDataTable({
@@ -158,7 +134,7 @@ shinyServer(function(input, output) {
                 colnames(a) <- c('Mean overall', 'Mean in Clust', 'fc', 'p-Value', 'adj. p-Value')
                 datatable(
                   a[a$fc<1,], selection = 'none', class = 'cell-border strip hover', options = list(autoWidth = TRUE)
-                ) %>% formatStyle(0, cursor = 'pointer') %>% 
+                ) %>% #formatStyle(0, cursor = 'pointer') %>% 
                   formatRound(c(1:5), 2) 
                 
         }, caption=paste("Downregulated Genes"),
